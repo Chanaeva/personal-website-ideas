8b1fe0a6cc3151b2a2141b87eef37802644f08fc
Chana Reynolds <chana777@gmail.com>
2017-05-12 12:20:50 -0600

    Added Layout and Color Scheme - color scheme - layout - font - added jQuery

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+$( document ).ready(function() {
+  $('.parallax').parallax();
+  console.log( "ready!" );
 
+});
+
+
+
+$( window ).on( "load", function() {
+       console.log( "window loaded" );
+   });
