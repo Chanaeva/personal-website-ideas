9e95fb231064ce493409d0cedda5cd37a1472b90
Chana Reynolds <chana777@gmail.com>
2017-05-13 10:24:17 -0600

    adjusted margins

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-$( document ).ready(function() {
-  $('.parallax').parallax();
-  console.log( "ready!" );
+// $( document ).ready(function() {
+//   // $('.parallax').parallax();
+//
+//
+//   console.log( "ready!" );
+//
+// });
 
-});
+$(document).ready(function(){
+    $('.scrollspy').scrollSpy();
+    console.log( "ready!" );
+  });
 
 
 
 $( window ).on( "load", function() {
+    $('#name-h1').addClass('animated fadeIn');
        console.log( "window loaded" );
    });
